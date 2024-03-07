33bcb9af7e88abf0f07c2d63365194670c45deb1
JulioChacon <jcchacon3000@gmail.com>
2024-03-06 23:24:06 -0700

    Mi primera api node

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+const logger = require('./loggerMiddleware')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(logger)
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body
+
+  if (!note || !note.content) {
+    return res.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    date: new Date().toISOString(),
+    important: typeof note.important !== 'undefined' ? note.important : false
+  }
+
+  notes = [...notes, newNote]
+  console.log(note)
+  res.status(201).json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  res.status(204).end()
+})
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not found'
+  })
+})
+
+app.listen(3001, () => {
+  console.log('Server is listening on port 3001')
+})
